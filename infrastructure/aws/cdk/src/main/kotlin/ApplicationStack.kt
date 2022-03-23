678aeb2b1ded4eefe17738a0800658a0505add08
Kevin Swiber <kswiber@gmail.com>
2022-03-23 15:26:02 -0700

    Increasing memory limit for AWS Lambda function.

--- infrastructure/aws/cdk/src/main/kotlin/ApplicationStack.kt
+++ infrastructure/aws/cdk/src/main/kotlin/ApplicationStack.kt
@@ -8,7 +8,7 @@ import software.amazon.awscdk.services.lambda.DockerImageFunction
 import software.constructs.Construct
 
 class ApplicationStack(scope: Construct, id: String, props: StackProps? = null) :
-        Stack(scope, id, props) {
+    Stack(scope, id, props) {
     val handler = "com.postman.securityscanner.infrastructure.aws.lambda.Handler::handleRequest"
     val prefix = "security-scanner"
 
@@ -16,45 +16,46 @@ class ApplicationStack(scope: Construct, id: String, props: StackProps? = null) 
         val stackSuffix = (node.tryGetContext("stackSuffix") as String?) ?: "main"
         val apiSuffix = (node.tryGetContext("apiSuffix") as String?) ?: stackSuffix
         val stageName =
-                (node.tryGetContext("stageName") as String?)
-                        ?: if (stackSuffix == "main") "prod" else "dev"
+            (node.tryGetContext("stageName") as String?)
+                ?: if (stackSuffix == "main") "prod" else "dev"
 
         val codeProps = AssetImageCodeProps.builder().cmd(listOf(handler)).build()
         val code = DockerImageCode.fromImageAsset("../lambda", codeProps)
         val function =
-                DockerImageFunction.Builder.create(this, "$prefix-function-$apiSuffix-$stageName")
-                        .code(code)
-                        .functionName("scanner")
-                        .timeout(Duration.minutes(15))
-                        .build()
+            DockerImageFunction.Builder.create(this, "$prefix-function-$apiSuffix-$stageName")
+                .code(code)
+                .functionName("scanner")
+                .timeout(Duration.minutes(15))
+                .memorySize(10240)
+                .build()
 
         val api =
-                RestApi.Builder.create(this, "$prefix-api-$apiSuffix")
-                        .restApiName("$prefix-api")
-                        .deploy(false)
-                        .build()
+            RestApi.Builder.create(this, "$prefix-api-$apiSuffix")
+                .restApiName("$prefix-api")
+                .deploy(false)
+                .build()
 
         val integration = LambdaIntegration.Builder.create(function).build()
 
         api.root.addMethod("ANY", integration)
         api.root.addResource("scan").addMethod("ANY", integration)
         api.root.addProxy(
-                ProxyResourceOptions.builder()
-                        .anyMethod(true)
-                        .defaultIntegration(integration)
-                        .build()
+            ProxyResourceOptions.builder()
+                .anyMethod(true)
+                .defaultIntegration(integration)
+                .build()
         )
 
         val deployment =
-                Deployment.Builder.create(this, "$prefix-deployment-$apiSuffix-$stageName")
-                        .api(api)
-                        .build()
+            Deployment.Builder.create(this, "$prefix-deployment-$apiSuffix-$stageName")
+                .api(api)
+                .build()
 
         val stage =
-                Stage.Builder.create(this, "$prefix-stage-$apiSuffix-$stageName")
-                        .stageName(stageName)
-                        .deployment(deployment)
-                        .build()
+            Stage.Builder.create(this, "$prefix-stage-$apiSuffix-$stageName")
+                .stageName(stageName)
+                .deployment(deployment)
+                .build()
 
         api.deploymentStage = stage
     }
